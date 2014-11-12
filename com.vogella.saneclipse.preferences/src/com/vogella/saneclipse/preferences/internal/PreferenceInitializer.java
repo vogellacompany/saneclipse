aea13cee02e46c1e51087a482c53cb3d47bd8d71
Lars Vogel <Lars.Vogel@vogella.com>
2014-11-12 13:06:03 +0100

    Activates the line number ruler in the editor
    Also introduces getNode and savePrefs methods, the existing code stills
    needs to be rewriten to cover that.

--- com.vogella.saneclipse.preferences/src/com/vogella/saneclipse/preferences/internal/PreferenceInitializer.java
+++ com.vogella.saneclipse.preferences/src/com/vogella/saneclipse/preferences/internal/PreferenceInitializer.java
@@ -9,67 +9,99 @@ public class PreferenceInitializer implements IStartup {
 	@Override
 	public void earlyStartup() {
 		configureJDT();
-		  configureResourceEncoding();
-		  configureLineSeparator();
-		  configureMemoryMonitorActive();
+		configureEditor();
+		configureResourceEncoding();
+		configureLineSeparator();
+		configureMemoryMonitorActive();
+	}
+
+	private void configureEditor() {
+		IEclipsePreferences prefs = getNode("org.eclipse.ui.editors");
+		prefs.putBoolean("lineNumberRuler", true);
+		savePrefs(prefs);
 	}
 
 	private void configureMemoryMonitorActive() {
 		// Platform.ui settings
-		  IEclipsePreferences platformuiprefs = InstanceScope.INSTANCE.getNode("org.eclipse.ui"); // does all the above behind the scenes
-		  
-		  platformuiprefs.putBoolean("SHOW_MEMORY_MONITOR", true);
-		  try {
-			  // prefs are automatically flushed during a plugin's "super.stop()".
-			  platformuiprefs.flush();
-		  } catch (org.osgi.service.prefs.BackingStoreException e) {
-			  e.printStackTrace();
-		  }
+		IEclipsePreferences platformuiprefs = InstanceScope.INSTANCE
+				.getNode("org.eclipse.ui"); // does all the above behind the
+											// scenes
+
+		platformuiprefs.putBoolean("SHOW_MEMORY_MONITOR", true);
+		try {
+			// prefs are automatically flushed during a plugin's "super.stop()".
+			platformuiprefs.flush();
+		} catch (org.osgi.service.prefs.BackingStoreException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private void configureLineSeparator() {
 		// Workspace settings
-		  IEclipsePreferences runtimeprefs = InstanceScope.INSTANCE.getNode("org.eclipse.core.runtime"); // does all the above behind the scenes
-		  
-		  runtimeprefs.put("line.separator", "\n");
-		  try {
-			  // prefs are automatically flushed during a plugin's "super.stop()".
-			  runtimeprefs.flush();
-		  } catch (org.osgi.service.prefs.BackingStoreException e) {
-			  e.printStackTrace();
-		  }
+		IEclipsePreferences runtimeprefs = InstanceScope.INSTANCE
+				.getNode("org.eclipse.core.runtime"); // does all the above
+														// behind the scenes
+
+		runtimeprefs.put("line.separator", "\n");
+		try {
+			// prefs are automatically flushed during a plugin's "super.stop()".
+			runtimeprefs.flush();
+		} catch (org.osgi.service.prefs.BackingStoreException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private void configureResourceEncoding() {
 		// Workspace settings
-		  IEclipsePreferences resourcesprefs = InstanceScope.INSTANCE.getNode("org.eclipse.core.resources"); // does all the above behind the scenes
-		  
-		  resourcesprefs.put("encoding", "UTF-8");
-		  try {
-			  // prefs are automatically flushed during a plugin's "super.stop()".
-			  resourcesprefs.flush();
-		  } catch (org.osgi.service.prefs.BackingStoreException e) {
-			  e.printStackTrace();
-		  }
+		IEclipsePreferences resourcesprefs = InstanceScope.INSTANCE
+				.getNode("org.eclipse.core.resources"); // does all the above
+														// behind the scenes
+
+		resourcesprefs.put("encoding", "UTF-8");
+		try {
+			// prefs are automatically flushed during a plugin's "super.stop()".
+			resourcesprefs.flush();
+		} catch (org.osgi.service.prefs.BackingStoreException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private void configureJDT() {
 		// JDT settings
-		  IEclipsePreferences prefs = InstanceScope.INSTANCE.getNode("org.eclipse.jdt.ui"); // does all the above behind the scenes
+		IEclipsePreferences prefs = InstanceScope.INSTANCE
+				.getNode("org.eclipse.jdt.ui"); // does all the above behind the
+												// scenes
 
-		  prefs.putInt("content_assist_autoactivation_delay", 20);
-		  prefs.put("content_assist_autoactivation_triggers_java", ".abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_");
-		  prefs.put("org.eclipse.jdt.ui.typefilter.enabled", "java.awt.*;javax.swing.*;");
-		  prefs.putBoolean("content_assist_insert_completion", false);
-		  prefs.putBoolean("enclosingBrackets", true);
-		  prefs.putBoolean("smart_semicolon", true);
-		  prefs.putBoolean("smart_opening_brace", true);
-		  try {
-		    // prefs are automatically flushed during a plugin's "super.stop()".
-		    prefs.flush();
-		  } catch (org.osgi.service.prefs.BackingStoreException e) {
+		prefs.putInt("content_assist_autoactivation_delay", 20);
+		prefs.put("content_assist_autoactivation_triggers_java",
+				".abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_");
+		prefs.put("org.eclipse.jdt.ui.typefilter.enabled",
+				"java.awt.*;javax.swing.*;");
+		prefs.putBoolean("content_assist_insert_completion", false);
+		prefs.putBoolean("enclosingBrackets", true);
+		prefs.putBoolean("smart_semicolon", true);
+		prefs.putBoolean("smart_opening_brace", true);
+		try {
+			// prefs are automatically flushed during a plugin's "super.stop()".
+			prefs.flush();
+		} catch (org.osgi.service.prefs.BackingStoreException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private IEclipsePreferences getNode(String node) {
+		// Platform.ui settings
+		return InstanceScope.INSTANCE.getNode(node);
+	}
+
+	private void savePrefs(IEclipsePreferences prefs) {
+		try {
+			// prefs are automatically flushed during a plugin's "super.stop()".
+			prefs.flush();
+		} catch (org.osgi.service.prefs.BackingStoreException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
