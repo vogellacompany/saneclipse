f79a35bf5d97b27579a3fdd79683728cda54f0c7
Simon Scholz <simon.scholz@vogella.com>
2014-11-17 20:58:56 +0100

    Also include save actions as default setting
    Change-Id: Ib0d7623d947caf240e19914b66478b15caaeae71
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.saneclipse.preferences/src/com/vogella/saneclipse/preferences/internal/PreferenceInitializerAddon.java
+++ com.vogella.saneclipse.preferences/src/com/vogella/saneclipse/preferences/internal/PreferenceInitializerAddon.java
@@ -13,8 +13,7 @@ public class PreferenceInitializerAddon {
 
 	@Inject
 	@Optional
-	public void name(
-			@UIEventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) Event event) {
+	public void name(@UIEventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) Event event) {
 		configureJDT();
 		configureEditor();
 		configureResourceEncoding();
@@ -30,9 +29,13 @@ public class PreferenceInitializerAddon {
 
 	private void configureMemoryMonitorActive() {
 		// Platform.ui settings
-		IEclipsePreferences platformuiprefs = InstanceScope.INSTANCE
-				.getNode("org.eclipse.ui"); // does all the above behind the
-											// scenes
+		IEclipsePreferences platformuiprefs = InstanceScope.INSTANCE.getNode("org.eclipse.ui"); // does
+																								// all
+																								// the
+																								// above
+																								// behind
+																								// the
+																								// scenes
 
 		platformuiprefs.putBoolean("SHOW_MEMORY_MONITOR", true);
 		try {
@@ -45,9 +48,13 @@ public class PreferenceInitializerAddon {
 
 	private void configureLineSeparator() {
 		// Workspace settings
-		IEclipsePreferences runtimeprefs = InstanceScope.INSTANCE
-				.getNode("org.eclipse.core.runtime"); // does all the above
-														// behind the scenes
+		IEclipsePreferences runtimeprefs = InstanceScope.INSTANCE.getNode("org.eclipse.core.runtime"); // does
+																										// all
+																										// the
+																										// above
+																										// behind
+																										// the
+																										// scenes
 
 		runtimeprefs.put("line.separator", "\n");
 		try {
@@ -60,9 +67,13 @@ public class PreferenceInitializerAddon {
 
 	private void configureResourceEncoding() {
 		// Workspace settings
-		IEclipsePreferences resourcesprefs = InstanceScope.INSTANCE
-				.getNode("org.eclipse.core.resources"); // does all the above
-														// behind the scenes
+		IEclipsePreferences resourcesprefs = InstanceScope.INSTANCE.getNode("org.eclipse.core.resources"); // does
+																											// all
+																											// the
+																											// above
+																											// behind
+																											// the
+																											// scenes
 
 		resourcesprefs.put("encoding", "UTF-8");
 		try {
@@ -75,25 +86,32 @@ public class PreferenceInitializerAddon {
 
 	private void configureJDT() {
 		// JDT settings
-		IEclipsePreferences prefs = InstanceScope.INSTANCE
-				.getNode("org.eclipse.jdt.ui"); // does all the above behind the
-												// scenes
-
+		IEclipsePreferences prefs = InstanceScope.INSTANCE.getNode("org.eclipse.jdt.ui"); // does
+																							// all
 		prefs.putInt("content_assist_autoactivation_delay", 20);
 		prefs.put("content_assist_autoactivation_triggers_java",
 				".abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_");
-		prefs.put("org.eclipse.jdt.ui.typefilter.enabled",
-				"java.awt.*;javax.swing.*;");
+		prefs.put("org.eclipse.jdt.ui.typefilter.enabled", "java.awt.*;javax.swing.*;");
 		prefs.putBoolean("content_assist_insert_completion", false);
 		prefs.putBoolean("enclosingBrackets", true);
 		prefs.putBoolean("smart_semicolon", true);
 		prefs.putBoolean("smart_opening_brace", true);
+		
+		configureSaveActions(prefs);
+		
 		try {
 			// prefs are automatically flushed during a plugin's "super.stop()".
 			prefs.flush();
 		} catch (org.osgi.service.prefs.BackingStoreException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void configureSaveActions(IEclipsePreferences prefs) {
+		prefs.putBoolean("editor_save_participant_org.eclipse.jdt.ui.postsavelistener.cleanup", true);
+		prefs.putBoolean("sp_cleanup.format_source_code_changes_only", true);
+		prefs.putBoolean("sp_cleanup.organize_imports", true);
+		prefs.putBoolean("sp_cleanup.on_save_use_additional_actions", true);
 	}
 
 	private IEclipsePreferences getNode(String node) {
